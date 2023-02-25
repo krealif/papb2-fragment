53d01b97ebca03d133edc7e64ca58b405f8d1d43
Shavana Alif <85614368+krealif@users.noreply.github.com>
2023-02-25 20:52:04 +0700

    initial commit

---
+++ app/src/main/java/com/example/tugasfragment/DescFragment.java
@@ -0,0 +1,24 @@
+package com.example.tugasfragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+public class DescFragment extends Fragment {
+    public DescFragment() {
+        // Required empty public constructor
+    }
+    public static DescFragment newInstance() {
+        return new DescFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_desc, container, false);
+    }
+}
