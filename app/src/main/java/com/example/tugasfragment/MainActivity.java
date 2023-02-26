13d3628b7eacd645529ab6774f97d82e46c3f020
Shavana Alif <85614368+krealif@users.noreply.github.com>
2023-02-26 07:11:17 +0700

    dynamic fragment

--- app/src/main/java/com/example/tugasfragment/MainActivity.java
+++ app/src/main/java/com/example/tugasfragment/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.tugasfragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+    private boolean isDescDisplayed = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment;
+                if(isDescDisplayed) {
+                    fragment = AdvFragment.newInstance();
+                    btn.setText(R.string.mantap);
+                } else {
+                    fragment = DescFragment.newInstance();
+                    btn.setText(R.string.lihat);
+                }
+                showFragment(fragment);
+                isDescDisplayed = !isDescDisplayed;
+            }
+        });
+
+        showFragment(DescFragment.newInstance());
+    }
+
+    private void showFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager
+                .beginTransaction();
+
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+                .addToBackStack(null)
+                .commit();
     }
 }
